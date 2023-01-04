9c9e4a026a0977942cd7d14e67c009718c6dd0dc
MuhammedGul34 <muhammedgul.mail@gmail.com>
2023-01-04 11:53:27 +0300

    Trending creators an d Popular places to eat

--- TravelDiscovery/ContentView.swift
+++ TravelDiscovery/ContentView.swift
@@ -26,7 +26,18 @@ struct ContentView: View {
     }
 }
 
+struct Destination: Hashable {
+    let name, country, imageName: String
+}
+
 struct PopularDestinationView: View {
+    
+    let destinations: [Destination] = [
+        .init(name: "Paris", country: "France", imageName: "eiffel_tower"),
+        .init(name: "Tokyo", country: "Japan", imageName: "japan"),
+        .init(name: "New York", country: "US", imageName: "new_york"),
+    ]
+    
     var body: some View{
         VStack{
             HStack{
@@ -40,10 +51,31 @@ struct PopularDestinationView: View {
             
             ScrollView(.horizontal) {
                 HStack(spacing: 8){
-                    ForEach(0..<5, id: \.self){ num in
-                        Spacer()
-                            .frame(width: 125, height: 150)
-                            .background(Color.gray)
+                    ForEach(destinations, id: \.self){ destination in
+                        VStack(alignment: .leading, spacing: 0){
+                            
+                            Image(destination.imageName)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 120, height: 120)
+//                                .clipped()
+                                .cornerRadius(4)
+                                .padding(.horizontal, 6)
+                                .padding(.vertical,6)
+                           
+                            Text(destination.name)
+                                .font(.system(size: 12, weight: .semibold))
+                                .padding(.horizontal, 12)
+                            
+                            
+                            Text(destination.country)
+                                .font(.system(size: 12, weight: .semibold))
+                                .padding(.horizontal, 12)
+                                .padding(.bottom, 8)
+                                .foregroundColor(.gray)
+                        }
+//                            .frame(width: 125)
+                            .background(Color(.init(white: 0.9, alpha: 1)))
                             .cornerRadius(10)
                             .shadow(color: .gray, radius: 5, x: 0.0, y: 2.0)
                             .padding(.bottom)
@@ -54,7 +86,17 @@ struct PopularDestinationView: View {
     }
 }
 
+struct restaurant: Hashable {
+    let name, imageName: String
+}
+
 struct PopularRestaurantsView: View {
+    
+    let restaurants: [restaurant] = [
+        .init(name: "Japan's Finest Tapas", imageName: "tapas"),
+        .init(name: "Bar & Grill", imageName: "bar_grill")
+    ]
+    
     var body: some View{
         VStack{
             HStack{
@@ -68,10 +110,42 @@ struct PopularRestaurantsView: View {
             
             ScrollView(.horizontal) {
                 HStack(spacing: 8){
-                    ForEach(0..<5, id: \.self){ num in
-                        Spacer()
-                            .frame(width: 210, height: 70)
-                            .background(Color.gray)
+                    ForEach(restaurants, id: \.self){ restaurant in
+                        HStack(spacing: 8){
+                            Image(restaurant.imageName)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 60, height: 60)
+                                .clipped()
+                                .cornerRadius(5)
+                                .padding(.leading, 6)
+                                .padding(.vertical, 6)
+                            
+                            VStack(alignment: .leading, spacing: 6){
+                                HStack{
+                                    Text(restaurant.name)
+                                    Spacer()
+                                    Button {
+                                        print("111")
+                                        // TODO: perform segur or whatever
+                                    } label: {
+                                        Image(systemName: "ellipsis")
+                                            .foregroundColor(.black)
+                                    }
+                                }
+                                HStack{
+                                    Image(systemName: "star.fill")
+                                    Text("4.7 * Sushi * $$")
+                                }
+                                Text("Tokyo, Japon")
+                                    
+                            }.font(.system(size: 12, weight: .semibold))
+                            
+                            Spacer()
+                                
+                        }
+                            .frame(width: 240)
+                            .background(Color(.init(white: 0.9, alpha: 1)))
                             .cornerRadius(5)
                             .shadow(color: .gray, radius: 5, x: 0.0, y: 2.0)
                             .padding(.bottom)
@@ -82,7 +156,18 @@ struct PopularRestaurantsView: View {
     }
 }
 
+struct User: Hashable {
+    let name, ImageName: String
+}
+
 struct TrendingCreatorsView: View {
+    
+    let users: [User] = [
+        .init(name: "Amy Adams", ImageName: "amy"),
+        .init(name: "Billy ", ImageName: "billy"),
+        .init(name: "Sam Smith", ImageName: "sam")
+    ]
+    
     var body: some View{
         VStack{
             HStack{
@@ -95,12 +180,19 @@ struct TrendingCreatorsView: View {
                 .padding(.top)
             
             ScrollView(.horizontal) {
-                HStack(spacing: 8){
-                    ForEach(0..<15, id: \.self){ num in
-                        Spacer()
-                            .frame(width: 58, height: 58)
-                            .background(Color.gray)
-                            .cornerRadius(.infinity)
+                HStack(alignment: .top, spacing: 12){
+                    ForEach(users, id: \.self){ user in
+                        VStack{
+                            Image(user.ImageName)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 60, height: 60)
+                                .cornerRadius(.infinity)
+                            Text(user.name)
+                                .font(.system(size: 11, weight: .semibold))
+                                .multilineTextAlignment(.center)
+                        }
+                        .frame(width: 60)
                             .shadow(color: .gray, radius: 5, x: 0.0, y: 2.0)
                             .padding(.bottom)
                     }
